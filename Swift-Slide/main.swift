89830b3da1bcdd99677dd6b9505863445a5c13b3
Omid-SH <hdomid@yahoo.com>
2020-06-21 12:03:16 -0700

    Initial Commit

---
+++ Swift-Slide/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Slide
+//
+//  Created by Omid on 6/21/20.
+//  Copyright © 2020 MobileProgramming. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
